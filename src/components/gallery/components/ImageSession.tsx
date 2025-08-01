674ca0b8fc9b9f264dd0037793457e5f6cfbbcde
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-08-01 20:28:48 +0530

    added : FAQs CMS

--- src/components/gallery/components/ImageSession.tsx
+++ src/components/gallery/components/ImageSession.tsx
@@ -1,24 +1,32 @@
-import imageOne from '../assets/Gallery1.jpg'
-import imageTwo from '../assets/gallery2.jpg'
-import imageThree from '../assets/gallery3.jpg'
-import imagefour from '../assets/gallery4.jpg'
-import imagefive from '../assets/gallery5.jpg'
-import imagesix from '../assets/gallery6.jpg'
-import imageseven from '../assets/gallery7.jpg'
+import imageOne from "../assets/Gallery1.jpg";
+import imageTwo from "../assets/gallery2.jpg";
+import imageThree from "../assets/gallery3.jpg";
+import imagefour from "../assets/gallery4.jpg";
+import imagefive from "../assets/gallery5.jpg";
+import imagesix from "../assets/gallery6.jpg";
+import imageseven from "../assets/gallery7.jpg";
+import imageeight from "../assets/gallery8.jpg";
+import imagenine from "../assets/gallery9.jpg";
+
+
 export const ImageSession = () => {
   return (
     <div className="pt-14 flex flex-col gap-4 items-center mobile:hidden sm:flex">
-        <img src={imageOne} alt="" />
-      <div className='flex gap-4'>
+      <img src={imageOne} alt="" />
+      <div className="flex gap-4">
         <img src={imageTwo} alt="" />
         <img src={imageThree} alt="" />
         <img src={imagefour} alt="" />
       </div>
       <img src={imagefive} alt="" />
-      <div className='flex gap-4'>
+      <div className="flex gap-4">
         <img src={imagesix} alt="" />
         <img src={imageseven} alt="" />
       </div>
+      <div className="flex gap-4">
+        <img src={imageeight} alt="" />
+        <img src={imagenine} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
