2c6cd58e754636f5eb2688d3c4949e92a12fbbd2
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-08-26 00:10:53 +0530

    Updated: Content | About Page

--- src/components/About/components/Hero.tsx
+++ src/components/About/components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = () => {
       <div className="relative z-10">
         <Header />
         <div className="text-[#ffff] font-staylista sm:text-[72px] h-[100vh] flex flex-col items-center justify-end mobile:text-5xl">
-          <h2 className="text-center">Designed by nature,</h2>
-          <h2 className="mb-[120px]">Sumptuously.</h2>
+          <span className="text-center">Kudajdri Drizzle</span>
+          <h2 className="mb-[120px] text-center">Your Ideal Choice for Homestays in Kalpetta</h2>
         </div>
       </div>
     </div>
