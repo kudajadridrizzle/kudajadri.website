96108890cd78b0d6246ac10ccf903505b01f52f9
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-08-26 21:58:05 +0530

    Update: Contact Us

--- src/components/Contact/ContactPage.tsx
+++ src/components/Contact/ContactPage.tsx
@@ -87,11 +87,11 @@ export const ContactPage = () => {
         <div className="flex flex-col sm:gap-[90px] gap-7">
           <div className="flex flex-col items-center gap-6">
             <span className="text-[#000] font-albertSans tracking-[1.6px] text-base">
-              Contact Us
+              CONTACT US{' '}
             </span>
-            <span className="sm:text-[72px] text-[32px] text-[#000] font-staylista">
-              Experience Kudajadri
-            </span>
+            <h1 className="sm:text-[72px] text-[32px] text-[#000] font-staylista text-center">
+              Easy Online Booking of Wayanad Homestays & Cottages
+            </h1>
           </div>
         </div>
 
