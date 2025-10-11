25ee07a9900a1eee35e2a88ad38a0a1b30f0de62
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-10-11 18:45:31 +0530

    optimisation & wayanad page update

--- src/components/Home/components/VideoBackground.tsx
+++ src/components/Home/components/VideoBackground.tsx
@@ -7,7 +7,7 @@ const VideoBackground = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-hidden bg-black">
       {/* Desktop Background */}
       <div className="hidden md:block absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-full z-0">
         {/* Hero Image */}
