9a1a1c763dbc7ae53dc15b04f967f00f8e105755
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-10-12 07:52:42 +0530

    optimization |room image sesssion | Home page

--- src/components/Home/components/RoomSession.tsx
+++ src/components/Home/components/RoomSession.tsx
@@ -11,13 +11,17 @@ const RoomSession = () => {
 
   const isLong = content.length > maxChars;
   const preview = isLong ? content.slice(0, maxChars) + '...' : content;
+
   return (
     <div className="sm:px-[12%] sm:pt-32 sm:pb-0 bg-[#FFF] px-4 mobile:pt-14 mobile:pb-6 large:px-[18%]">
       <div className="sm:py-12 flex flex-col sm:flex-row justify-center">
-        <h2 className="flex-1 sm:text-[44px] text-[32px] font-ivy">
+        {/* Heading */}
+        <h2 className="flex-1 sm:text-[44px] text-[32px] font-ivy mb-4 sm:mb-16">
           {heading}
         </h2>
-        <div className="sm:w-[466px] sm:text-xl font-albertSans text-secondary">
+
+        {/* Paragraph */}
+        <div className="sm:w-[466px] sm:text-xl font-albertSans text-secondary sm:ml-16">
           <ReactMarkdown remarkPlugins={[remarkGfm]}>
             {expanded || !isLong ? content : preview}
           </ReactMarkdown>
